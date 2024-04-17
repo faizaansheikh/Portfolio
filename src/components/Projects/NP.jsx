8c789fe017a08ee80ceb45372b14771f737e0bf6
Faizaan_sheikh <princefaizan800@gmail.com>
2024-04-17 14:46:30 +0500

    Update NP.jsx

--- src/components/Projects/NP.jsx
+++ src/components/Projects/NP.jsx
@@ -1,5 +1,5 @@
 import { Box, Button, Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import "./projects.css";
 import pic1 from './images/expense1.JPG'
 import LinkIcon from '@mui/icons-material/Link';
@@ -10,22 +10,24 @@ import car from "./images/ssc.png";
 import wether12 from "./images/wether12.JPG";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { FAColor } from "../../theme";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 function NP() {
     const boxdata = [
         {
             id: 1, urls: "https://automobile-rental-app-psi.vercel.app/", image: car, title: "Car Rental App",
-            links: 'https://github.com/faizaansheikh/library_app', arr: ['Material UI', 'Javascript', 'React','Node','MongoDB'],
+            links: 'https://github.com/faizaansheikh/library_app', arr: ['Javascript', 'React','Node','MongoDB','Express','Material UI'],
             des:'Introducing our car renting system: effortlessly book the perfect car for any occasion. This platform offers a diverse selection of vehicles with detailed descriptions and photos. Simply choose, book, and go! our React-powered admin panel streamlines operations. Manage bookings, update listings, and monitor performance from one centralized hub'
         },
         {
             id: 2, urls: "https://weather-a55d2.web.app/", image: wether, title: "Weather App ",
-            links: 'https://github.com/faizaansheikh/Weatherapp', arr: ['HTML', 'CSS', 'Javascript', 'React'],
-            des:"Explore the world's weather with our innovative Weather App, meticulously crafted using React and integrated with a robust API. This user-friendly application provides real-time weather updates, forecasts, and intuitive visuals, all tailored for your convenience"
+            links: 'https://github.com/faizaansheikh/Weatherapp', arr: ['HTML', 'CSS', 'Material UI','Javascript', 'React'],
+            des:"Explore the world's weather with our innovative Weather App, meticulously crafted using React and integrated with a robust API. This user-friendly application provides real-time weather updates, forecasts, and intuitive visuals, all tailored for your convenience our responsive design ensures a seamless experience across devices. "
         },
         {
             id: 3, urls: "https://xresort-53b181.netlify.app", image: hotel, title: "Hotel Website UI",
             links: 'https://github.com/faizaansheikh/hotelweb', arr: ['HTML', 'CSS', 'React'],
-            des:"A seamless navigation and stunning design with our website UI, meticulously crafted using React. This dynamic interface features multiple responsive pages, ensuring an optimal viewing experience across devices. Explore a user-friendly interface designed to enhance your browsing journey"
+            des:"A seamless navigation and stunning design with our website UI, meticulously crafted using React. This dynamic interface features multiple responsive pages, ensuring an optimal viewing experience across devices. Explore a user-friendly interface designed to enhance your browsing journey. Explore the convergence of functionality and aesthetics as you delve into our meticulously designed interface."
         },
         {
             id: 4, urls: "https://expenseappx.netlify.app/", image: expense, title: "Expense App ",
@@ -47,7 +49,7 @@ function NP() {
         fontFamily: "'Oswald', sans-serif",
         fontFamily: "'Teko', sans-serif",
         fontFamily: "'ZCOOL QingKe HuangYou', sans-serif",
-        letterSpacing: "2px",
+        letterSpacing: "1px",
         color: "#fff",
 
         fontSize: "30px",
@@ -56,9 +58,15 @@ function NP() {
         pt: '27px'
     };
     const handleUrl = (links)=>{
-        console.log(links);
+      
         window.open(links,'_blank')
     }
+    useEffect(() => {
+        AOS.init({
+          duration: 1000, // Animation duration
+          easing: 'ease-in-out', // Animation easing
+        });
+      }, []);
     return (
         <>
             <div className="projects-section" id="Projects">
@@ -73,19 +81,21 @@ function NP() {
                     </Typography>
                 </Box>
                 <Box sx={{ p: '50px' }}>
-                    <Grid container spacing={4}>
+                    <Grid container spacing={4} sx={{height:'auto'}}>
                         {boxdata.map((el, i) => (
-                            <Grid key={i} item xs={12} md={4} sx={{ height: 'auto','&:hover':{cursor:'pointer'}, }} onClick={()=>handleUrl(el.urls)}>
-                                <Box sx={{ border: '1px solid grey', borderRadius: '10px', '&:hover': { border: '1.3px solid white',boxShadow: '0 30px 60px 30px black' }, transition: '0.3s ease' }}>
+                            <Grid data-aos={i%2 === 0 ? 'fade-right':'fade-left'} key={i} item xs={12} md={4} sx={{ height: 'auto','&:hover':{cursor:'pointer'}, }} onClick={()=>handleUrl(el.urls)}>
+                                <Box sx={{ height:'auto',border: '1px solid grey', borderRadius: '10px', '&:hover': { border: '1.3px solid white',boxShadow: '0 30px 60px 30px black' }, transition: '0.3s ease' }}>
                                     <img src={el.image} style={{ width: '100%', height: '250px', filter:'brightness(85%)',objectFit: 'cover', borderTopRightRadius: '10px', borderTopLeftRadius: '10px' }} alt="" />
-                                    <Box sx={{ p: '15px 15px 0px 15px', height: '235px',width:'100%' }}>
+                                    <Box sx={{ p: '15px 15px 0px 15px' }}>
                                         <Typography sx={{ color: 'white' }} variant="h5">{el.title}</Typography>
                                         <Typography sx={{ color: 'lightgrey', fontSize: "12px" }}>{el.des}</Typography>
-                                        <Box sx={{ display: 'flex', mt: '10px' ,width:'60px'}}>
+                                        <Grid container spacing={1} sx={{ display: 'flex', alignItems:'center',mt: '10px' }}>
                                           
-                                           {el.arr?.map((e,ind)=>( <Box sx={{ borderRadius: '3px',color: 'white', px: '8px', py: '5px', fontSize: '11px', mr: '9px', bgcolor: '#383d38' ,textWrap:'nowrap'}}>{e} </Box>))}
+                                           {el.arr?.map((e,ind)=>( <Grid data-aos='fade-up' item xs={3} md={3} key={ind} sx={{ display: 'flex', alignItems:'center',justifyContent:'center'}}>
+                                            <Box sx={{ display:'flex',justifyContent:'center',alignItems:'center',borderRadius: '3px',color: 'white',p:'4px 5px 4px 5px',fontSize: '11px',width:'100%', bgcolor: '#383d38' ,textWrap:'nowrap'}}>{e}</Box>
+                                             </Grid>))}
                                           
-                                        </Box>
+                                        </Grid>
                                         <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: '10px', mb: '5px',alignItems:'center' }}>
                                             <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                                                 <Typography sx={{ color: 'grey' }}><LinkIcon /> </Typography>
